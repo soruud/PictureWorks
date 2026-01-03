70681e54a779437873fedc2238fefbcb6a2dbb34
soruud <soruud@gmail.com>
2026-01-03 16:07:34 +0100

    v1.2.1: Fix SplashScreen - remove problematic image loading code, use SizeToContent instead

--- SplashScreen.xaml.cs
+++ SplashScreen.xaml.cs
@@ -1,6 +1,5 @@
 using System.Windows;
 using System.Windows.Threading;
-using System.Windows.Media.Imaging;
 
 namespace PictureWorks;
 
@@ -14,25 +13,6 @@ public partial class SplashScreen : Window
     public SplashScreen()
     {
         InitializeComponent();
-        
-        // Load image to get its dimensions and set window size
-        try
-        {
-            BitmapImage logo = new();
-            logo.BeginInit();
-            logo.UriSource = new Uri("pack://application:,,,/PW_LOGO.png");
-            logo.EndInit();
-            
-            // Set window size to match image size
-            this.Width = logo.PixelWidth;
-            this.Height = logo.PixelHeight;
-        }
-        catch
-        {
-            // Fallback size if image can't be loaded
-            this.Width = 500;
-            this.Height = 300;
-        }
         
         // Create timer to close splash screen after 4.5 seconds
         _timer = new DispatcherTimer
