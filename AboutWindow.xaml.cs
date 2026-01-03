cbe2492cb8ef5b172272e74cb27f15af52ffe781
soruud <soruud@gmail.com>
2026-01-03 14:49:49 +0100

    v1.1.4: Fix AboutWindow StaticResource error - define resources locally

--- AboutWindow.xaml.cs
+++ AboutWindow.xaml.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using System.Windows;
-using System.Windows.Media;
 
 namespace PictureWorks;
 
@@ -12,22 +11,6 @@ public partial class AboutWindow : Window
     public AboutWindow()
     {
         InitializeComponent();
-        
-        // Share theme resources from MainWindow
-        if (Application.Current.MainWindow is MainWindow mainWindow)
-        {
-            this.Resources.MergedDictionaries.Add(mainWindow.Resources);
-        }
-        else
-        {
-            // Fallback: Use default theme colors
-            Resources["WindowBackground"] = new SolidColorBrush(Color.FromRgb(240, 244, 248));
-            Resources["PanelBackground"] = new SolidColorBrush(Color.FromRgb(232, 237, 242));
-            Resources["ButtonBackground"] = new SolidColorBrush(Color.FromRgb(74, 144, 226));
-            Resources["ButtonHover"] = new SolidColorBrush(Color.FromRgb(53, 122, 189));
-            Resources["TextColor"] = new SolidColorBrush(Color.FromRgb(44, 62, 80));
-            Resources["BorderColor"] = new SolidColorBrush(Color.FromRgb(189, 195, 199));
-        }
     }
     
     private void BtnPayPal_Click(object sender, RoutedEventArgs e)
